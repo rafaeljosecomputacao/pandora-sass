77676a9ab65c49d0929d01e83b155b198ab66dff
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-12-29 19:45:36 -0300

    create-bottom-navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// Bottom navbar toggle menu
+function NavbarToggle() {
+    let navbarItems = document.querySelector('.bottom-navbar-items');
+    
+    if (navbarItems.classList.contains('open')) {
+        navbarItems.classList.remove('open');
+        document.querySelector('.bottom-navbar-icon').innerHTML = `<i class="bi bi-list"></i>`;
+    } else {
+        navbarItems.classList.add('open');
+        document.querySelector('.bottom-navbar-icon').innerHTML = `<i class="bi bi-x"></i>`;
+    }
+}
+
 // Current year for footer
 var year = new Date();
 document.getElementById('year').innerHTML = year.getFullYear();
